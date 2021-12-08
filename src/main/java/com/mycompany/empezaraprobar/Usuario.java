1ed8c08fc1087dfde05d22f85089ee05b9042399
AxelDeRosa <axelnderosa@gmail.com>
2021-12-08 09:13:42 -0300

    Update Usuario.java
    Agregue el atributo edad, los constructores, get y ser,  y el toString

--- src/main/java/com/mycompany/empezaraprobar/Usuario.java
+++ src/main/java/com/mycompany/empezaraprobar/Usuario.java
@@ -12,4 +12,45 @@ package com.mycompany.empezaraprobar;
 public class Usuario {
     private String nombre;
     private String apellido;
+    private Integer edad;
+
+    public Usuario(String nombre, String apellido, Integer edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    public Usuario() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + '}';
+    }
+    
+    
 }
