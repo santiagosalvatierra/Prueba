70cdabc2f9ae45291505d448de9fdee9b8aee525
Santiago Salvatierra <santiagosalvartierra11@gmail.com>
2021-12-07 16:55:52 -0800

    Para practicar git antes del proyecto final

---
+++ src/main/java/com/mycompany/empezaraprobar/Usuario.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.empezaraprobar;
+
+/**
+ *
+ * @author Usuario 1
+ */
+public class Usuario {
+    private String nombre;
+    private String apellido;
+}
