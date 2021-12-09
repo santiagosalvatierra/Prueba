bfe38de6bc21effa3c27cc36b6329e4d39337a72
AxelDeRosa <axelnderosa@gmail.com>
2021-12-09 20:25:50 -0300

    saludando a la agu
    para probar
    
    Co-Authored-By: agumey_ <57041890+agumey@users.noreply.github.com>

---
+++ src/main/java/com/mycompany/entities/Usuario.java
@@ -0,0 +1,37 @@
+package com.mycompany.entities;
+
+/*
+hola agus xd
+ */
+
+public class Usuario {
+    private String nombre;
+    private String apellido;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    
+}
